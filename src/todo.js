755465c9fa590e3cd632c9abf082557c75f08af9
Pierre V <6772696d@gmail.com>
2024-09-11 18:23:01 +0200

    Make constructor for todo

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,13 @@
 export default class Todo {
+    
+    constructor(name, desc, dueDate, reqs, priority, isDone = false) {
+        this.name = name;
+        this.desc = desc;
+        this.dueDate = dueDate;
+        this.reqs = reqs;
+        this.priority = priority;
+        this.isDone = isDone;
+    }
+ 
 
 };
