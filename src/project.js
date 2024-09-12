32709ea2b52463bfbcf01812c1e4be84802653ef
Pierre V <6772696d@gmail.com>
2024-09-12 10:43:33 +0200

    Add type checking to project constructor

--- src/project.js
+++ src/project.js
@@ -1,7 +1,26 @@
+import TodoList from "./todoList";
+import isValidString from "./util/isValidString";
 export default class Project {
+    // Constructor
+    // name must be a string,
+    // todoList must be a todoList object
     constructor(name, todoList, dueDate) {
-        this.name = name;
-        this.todoList = todoList;
-        this.dueDate = dueDate;
+        if (isValidString(name)) {
+            this.name = name; 
+        } else {
+            throw new Error("Invalid name.");
+        }
+
+        if (todoList instanceof TodoList) {
+            this.todoList = todoList;
+        } else {
+            throw new Error("todoList must be a TodoList object.");
+        }
+
+        if (true) {
+            this.dueDate = dueDate;
+        } else {
+            throw new Error("dueDate must be a Date object.");
+        }
     }
 };
