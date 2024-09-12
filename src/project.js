3241e8668fb0042cd154a53d56a01576ae8f5efd
Pierre V <6772696d@gmail.com>
2024-09-12 08:32:51 +0200

    Make project class

--- src/project.js
+++ src/project.js
@@ -1,5 +1,7 @@
-import TodoList from "./todoList";
-
 export default class Project {
-
+    constructor(name, todoList, dueDate) {
+        this.name = name;
+        this.todoList = todoList;
+        this.dueDate = dueDate;
+    }
 };
