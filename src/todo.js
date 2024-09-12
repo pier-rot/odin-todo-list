05dd0bdfd59c5d74f98ebcd47cce2779c5d1f923
Pierre V <6772696d@gmail.com>
2024-09-12 09:38:41 +0200

    Add type checking to constructor

--- src/todo.js
+++ src/todo.js
@@ -1,32 +1,59 @@
 import isArraysEqual from "./util/arrayEqual";
-
+import isValidString from "./util/isValidString";
 export default class Todo {
 	constructor(name, desc, dueDate, reqs, priority, isDone = false) {
-		this.name = name;
-		this.desc = desc;
+		// Check if name is a valid string
+		if (isValidString(name)) {
+			this.name = name;
+		} else {
+			console.log(`${name} is not a valid name.`);
+		}
+        // Check if desc is a valid description
+		if (isValidString(desc)) {
+			this.desc = desc;
+		} else {
+			console.log(`${desc} is not a valid description.`);
+		}
+        // Check if dueDate is a valid date.
 		this.dueDate = dueDate;
-		this.reqs = reqs;
-		this.priority = priority;
-		this.isDone = isDone;
+  
+        // Check if reqs is an array.
+		if (reqs instanceof Array) {
+			this.reqs = reqs;
+		} else {
+			console.log(`${reqs} must be an array.`);
+		}
+        // Check if priority is a valid number between 1 and 5.
+		if (typeof priority !== "number") {
+			console.log(`${priority} must be a number.`);
+		} else if (priority > 5 || priority < 1) {
+			console.log(`${priority} must be between 1 and 5.`);
+		} else {
+			this.priority = priority;
+		}
+        // Check if isDone is a boolean.
+        if (typeof isDone !== "boolean") {
+            console.log(`${isDone} must be a boolean.`)
+        } else {
+		    this.isDone = isDone;
+        }
 	}
 
 	toggleStatus() {
 		this.isDone = !this.isDone;
-	};
+	}
 
-    isEqual(other) {
-        for(const key in this){
-            if (this[key] instanceof Array) {
-                if (!isArraysEqual(this[key], other[key])){
-                    return false
-                }
-            }
-            else if (this[key] != other[key]) {
-                console.log(`${key} : ${this[key]} !== ${other[key]}`);
-                return false;
-            }
-        }
-        return true;
-
-    }
-};
+	isEqual(other) {
+		for (const key in this) {
+			if (this[key] instanceof Array) {
+				if (!isArraysEqual(this[key], other[key])) {
+					return false;
+				}
+			} else if (this[key] != other[key]) {
+				console.log(`${key} : ${this[key]} !== ${other[key]}`);
+				return false;
+			}
+		}
+		return true;
+	}
+}
