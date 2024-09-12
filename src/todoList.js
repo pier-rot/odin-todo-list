5ad608aae02be39f36b0de36dc034fe336fa0ae6
Pierre V <6772696d@gmail.com>
2024-09-12 10:17:21 +0200

    Add type checking

--- src/todoList.js
+++ src/todoList.js
@@ -1,33 +1,40 @@
 import isArrayOf from "./util/isArrayOf";
 import Todo from "./todo";
 export default class TodoList {
-    constructor(todos){
-        this.todos = todos;
-        this.isEmpty = (todos.length == 0) ? true : false;
-    };
-
-    // Removes todo from the todo list.
-    remove(todo) {
-        if (this.isEmpty) {
-            this.todos = [];
-            console.log("You cannot remove from the todo list since it's empty.")
+	constructor(todos) {
+		if (isArrayOf(todos, Todo)) {
+			this.todos = todos;
+			this.isEmpty = todos.length == 0 ? true : false;
+		} else {
+            console.log(`${todos} must be an array of Todo objects.`)
         }
-        this.todos = this.todos.filter(
-            (elem) => {
-                return !(todo.isEqual(elem));
-            }
-        );
-        this.updateEmpty();
-    }
+	}
 
+	// Removes todo from the todo list.
+	remove(todo) {
+		if (this.isEmpty) {
+			this.todos = [];
+			console.log(
+				"You cannot remove from the todo list since it's empty."
+			);
+		}
+		this.todos = this.todos.filter((elem) => {
+			return !todo.isEqual(elem);
+		});
+		this.updateEmpty();
+	}
 
-    // Add todo to the todo list.
-    add(todo) {
-        this.todos.push(todo);
-        this.updateEmpty();
-    }
+	// Add todo to the todo list.
+	add(todo) {
+        if (todo instanceof Todo) {
+            this.todos.push(todo);
+            this.updateEmpty();
+        } else {
+            console.log(`${todo} is not a Todo object.`);
+        }
+	}
 
-    updateEmpty() {
-        this.isEmpty = (this.todos.length == 0) ? true : false;
-    }
-};
+	updateEmpty() {
+		this.isEmpty = this.todos.length == 0 ? true : false;
+	}
+}
