c01fb2be77c5f3c4942dbcf382e1048dde5e199a
Pierre V <6772696d@gmail.com>
2024-09-12 07:16:07 +0200

    Define todo class

--- src/todo.js
+++ src/todo.js
@@ -1,13 +1,14 @@
 export default class Todo {
-    
-    constructor(name, desc, dueDate, reqs, priority, isDone = false) {
-        this.name = name;
-        this.desc = desc;
-        this.dueDate = dueDate;
-        this.reqs = reqs;
-        this.priority = priority;
-        this.isDone = isDone;
-    }
- 
+	constructor(name, desc, dueDate, reqs, priority, isDone = false) {
+		this.name = name;
+		this.desc = desc;
+		this.dueDate = dueDate;
+		this.reqs = reqs;
+		this.priority = priority;
+		this.isDone = isDone;
+	}
 
+	toggleStatus() {
+		this.isDone = !this.isDone;
+	}
 };
