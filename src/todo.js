c3ad25e0c050ac6cc2add44e3288cd6e8f5c397d
Pierre V <6772696d@gmail.com>
2024-09-12 08:01:28 +0200

    Add method to check if two todos are equal

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,5 @@
+import isArraysEqual from "./util/arrayEqual";
+
 export default class Todo {
 	constructor(name, desc, dueDate, reqs, priority, isDone = false) {
 		this.name = name;
@@ -10,5 +12,21 @@ export default class Todo {
 
 	toggleStatus() {
 		this.isDone = !this.isDone;
-	}
+	};
+
+    isEqual(other) {
+        for(const key in this){
+            if (this[key] instanceof Array) {
+                if (!isArraysEqual(this[key], other[key])){
+                    return false
+                }
+            }
+            else if (this[key] != other[key]) {
+                console.log(`${this[key]} !== ${other[key]}`);
+                return false;
+            }
+        }
+        return true;
+
+    }
 };
