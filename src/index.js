0993dedd56720d7a6fd80dc7443137cf56c8e261
Pierre V <6772696d@gmail.com>
2024-09-13 10:07:53 +0200

    Add some small console logs to test functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import Todo from "./todo";
 import TodoList from "./todoList";
 import Project from "./project";
+import { readProjects, saveProjects } from "./util/jsonUtils";
 
-const todo1 = new Todo("todo1", "my desc", "date", [""], 2);
-const todo2 = new Todo("todo2", "my desc", "date", [""], 2);
-const todo3 = new Todo("todo3", "my desc", "date", [""], 2);
+const todo1 = new Todo("todo1", "my desc", new Date(), [""], 2);
+const todo2 = new Todo("todo2", "my desc", new Date(), [""], 2);
+const todo3 = new Todo("todo3", "my desc", new Date(), [""], 2);
 const tl = new TodoList([todo1, todo2, todo3]);
-const project = new Project("my project", tl, "date");
+const project = new Project("my project", tl, new Date());
+const project2 = new Project("project 2", tl, new Date());
+
+let projectList = [project, project2];
+// saveProjects(projectList,"localStorage");
+console.log(readProjects("localStorage"));
