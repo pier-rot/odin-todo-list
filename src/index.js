d679f7764a35112143be6817a1ca660e7b7195a0
Pierre V <6772696d@gmail.com>
2024-09-13 08:05:37 +0200

    Add dummy project variable for testing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import Todo from "./todo";
 import TodoList from "./todoList";
 import Project from "./project";
+
+const todo1 = new Todo("todo1", "my desc", "date", [""], 2);
+const todo2 = new Todo("todo2", "my desc", "date", [""], 2);
+const todo3 = new Todo("todo3", "my desc", "date", [""], 2);
+const tl = new TodoList([todo1, todo2, todo3]);
+const project = new Project("my project", tl, "date");
